fd22eeabe56778b3dac4978bea9386d34f5b18dc
Ayozé Design <47169371+10101995@users.noreply.github.com>
2022-03-21 17:51:22 +0000

    commit compteur ok

--- index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function message() {
-    return 'serge';
-}
-message();
-console.log(message());
-//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,41 @@
-console.log("Hello Sergio")
+// const a: string = "Hello World"
+// const b: number = 45
+// const c: boolean = false
+// const d: null = null
+// const arr: string[] = ["Hello World", "Sergio", "Pedri"]
+//
+// const users: {firstname: string, lastname: string, age: number} = {
+//     firstname: "Serginho",
+//     lastname: "Zoto",
+//     age: 27
+// }
+//
+// const date:Date = new Date();
+//
+// const cb:(e: MouseEvent) => void = (e: MouseEvent): number => {
+//     return 3
+// }
+//
+// const compteur = document.querySelector('#compteur')
+//
+// function printId(id: string | number): void {
+//     console.log(id.toString())
+// }
+// printId(3)
+
+const compteur = document.querySelector('#compteur')
+
+let i = 0;
+
+const increment = (e: Event) => {
+    e.preventDefault()
+    i+=5
+    const span = compteur?.querySelector('span')
+    if (span){
+        span.innerText = i.toString();
+    }
+}
+
+compteur?.addEventListener('click', increment)
+
+//console.log(i)
